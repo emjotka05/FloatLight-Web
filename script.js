89fcbd65dbaf5872d19b17509bfa36a5e30051b6
Max <emjotka05@gmail.com>
2026-03-29 13:53:39 +0200

    Feat/Add js basic logic for payment button

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/*
+const actionButtons = document.querySelectorAll(".action-btn")
+
+actionButtons.forEach(actionButton => {
+    actionButton.addEventListener("click", ()=> {
+        if(actionButton.id === "stripe-btn"){
+            const tab = window.open("https://buy.stripe.com/test_eVq4gz9Xb7FK6cX4Y8gMw00", '_blank')
+        }else{
+            window.location.href ="store.html"
+        }
+    })
+
+*/ 
