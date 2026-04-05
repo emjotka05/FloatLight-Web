1054eb397de181978c6d35b5923edca27529168f
Max <emjotka05@gmail.com>
2026-04-06 01:26:41 +0200

    Feat/integrate PostgreSQL with Stripe webhook

---
+++ database.js
@@ -0,0 +1,13 @@
+const dbQueries = {
+    queryAddCustomer: '',
+    queryAddOrder: ''
+}
+
+async function dbAddCustomer(params) {
+    
+}
+async function dbAddOrder(params) {
+    
+}
+
+module.exports = { dbAddCustomer, dbAddOrder }
--- server.js
+++ server.js
@@ -3,13 +3,32 @@ require('dotenv').config() //for env
 const stripeKey = process.env.STRIPE_SECRET_KEY
 const webhookKey = process.env.WEBHOOK_SECRET_KEY
 const priceID = process.env.FLOAT_LIGHT_PRICE_ID
+const db_user = process.env.DB_USER 
+const db_password = process.env.DB_PASSWORD
+const db_host = process.env.DB_HOST
+const db_port = process.env.DB_PORT
+const db_name = process.env.DB_NAME
 
 const express = require('express') //server
 const cors = require('cors') //cross-origin-reasource-sharing
+
 const { sendMailFunc } = require('./mailer.js')
 const stripe = require('stripe')(stripeKey)
 
+const { Pool } = require('pg')
+const pool = new Pool({
+    user: db_user,
+    host: db_host,
+    database: db_name,
+    password: db_password,
+    port: db_port
+})
+
 const app = express() //uruchamiamy silnik serwera
+
+pool.query('SELECT NOW()', (err, res) => {
+    if(err){console.log(err.message)}else{console.log("PostgreSQL database connected.")}
+})
 
 app.use(cors())
 
@@ -27,10 +46,21 @@ app.post('/webhook', express.raw({ type: 'application/json' }), async (req, res)
         if (event.type === 'checkout.session.completed') {
             let session = event.data.object
             console.log(session)
+            let sessionId = session.id
             let customerEmail = session.customer_details.email
             let customerName = session.customer_details.name
-
+            const lineItems = await stripe.checkout.sessions.listLineItems(sessionId)
+            let productQuantity = lineItems.data[0].quantity
+            const customerNameSplit = customerName.split(' ')
+            const queryAddCustomer = 'insert into Customers (first_name, last_name, email) values ($1, $2, $3) returning customer_id'
+            const valuesCustomer = [customerNameSplit[0], customerNameSplit[1], customerEmail]
+            const resultCustomer = await pool.query(queryAddCustomer, valuesCustomer)
+            const queryAddOrder = 'insert into Orders (customer_id, product_id, quantity, status, session_id) values ($1, $2, $3, $4, $5) returning order_id'
+            const valuesOrder = [resultCustomer.rows[0].customer_id, 1, productQuantity, 'paid',sessionId]
+            const resultOrder = await pool.query(queryAddOrder, valuesOrder)
             sendMailFunc(customerName, customerEmail)
+
+            //TODO clean server.js and add database.js
         }
         res.status(200).send()
 
@@ -47,8 +77,13 @@ app.post('/checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
-                price: `${priceID}`,
+                price: priceID,
                 quantity: 1,
+                adjustable_quantity: { 
+                    enabled: true,
+                    minimum: 1,
+                    maximum: 99,
+                },
             },
         ],
         shipping_address_collection: {
@@ -58,6 +93,7 @@ app.post('/checkout-session', async (req, res) => {
         success_url: `http://localhost:3000/success.html`,
         cancel_url: `http://localhost:3000/cancel.html`,
         automatic_tax: { enabled: true },
+        
     })
     res.json({ url: session.url })
 })
